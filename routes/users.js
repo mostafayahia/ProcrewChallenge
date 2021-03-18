18a1dafc516caca642c162fed3f706c2e29eca60
Yahia Hashem El-Tayeb <yahia.eltaib@yahoo.com>
2021-03-18 23:34:15 +0200

    feat: implement POST /users endpoint

--- routes/users.js
+++ routes/users.js
@@ -17,6 +17,36 @@ userRouter.route('/')
         res.json(results);
       }
     });
-  });
-
+  })
+  .post((req, res, next) => {
+    const user = req.body;
+    if (user && user.email && user.phone && user.name) {
+      connection.query(
+        'INSERT INTO users (name, email, phone) VALUES (?, ?, ?);',
+        [user.name, user.email, user.phone],
+        (err, results) => {
+          if (err) {
+            next(err);
+          } else {
+            connection.query(
+              `SELECT * FROM users WHERE id = ${results.insertId};`, 
+              (err, results) => {
+                if (err) {
+                  next (err);
+                } else {
+                  res.statusCode = 200;
+                  res.setHeader('Content-Type', 'application/json');
+                  res.json(results);
+                }
+              }
+            )
+          }
+        }
+      )
+    } else {
+      res.statusCode = 400;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Bad Request. Please provide all user data (name, email, phone)')
+    }
+  })
 module.exports = userRouter;
