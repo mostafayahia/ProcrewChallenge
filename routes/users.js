8706b4392d5668c35156ffa255a4c26c04a09a0f
Yahia Hashem El-Tayeb <yahia.eltaib@yahoo.com>
2021-03-18 23:06:44 +0200

    refactor: rename router -> userRouter

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,20 @@
 const express = require('express');
-const router = express.Router();
+const userRouter = express.Router();
 
 const connection = require('../database');
 
 /* GET users listing. */
-router.get('/', (req, res, next) => {
-  connection.query('SELECT * FROM users;', (err, results) => {
-    if (err) {
-      next(err);
-    } else {
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'application/json');
-      res.json(results);
-    }
+userRouter.route('/')
+  .get((req, res, next) => {
+    connection.query('SELECT * FROM users;', (err, results) => {
+      if (err) {
+        next(err);
+      } else {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(results);
+      }
+    });
   });
-});
 
-module.exports = router;
+module.exports = userRouter;
